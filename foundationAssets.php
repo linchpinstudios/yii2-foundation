64763bb6a1c470870e64432febabb67ac3fb3318
Josh Hagel <joshhagel@linchpinstudios.com>
2014-07-05 13:54:08 -0700

    Init Setup

---
+++ foundationAssets.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @link http://www.linchpinstudios.com/
+ * @copyright Copyright (c) 2014 Linchpin Studios LLC
+ * @license http://opensource.org/licenses/MIT
+ */
+
+namespace yii\bootstrap;
+
+use yii\web\AssetBundle;
+
+/**
+ * Asset bundle for the Twitter bootstrap css files.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class FoundationAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/linchpin-studios/yii2-foundation/dist';
+    public $css = [
+        'css/normalize.css',
+        'css/foundation.min.css',
+    ];
+    public $js = [
+        'js/vendor/modernizr.js',
+    ];
+}
